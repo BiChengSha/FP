bd895dc5dee48186e065865a452851c31d11042b
DavidRadzikovsky <sagadavid@gmail.com>
2015-01-01 13:45:13 -0500

    Create Railroad.java

---
+++ GameCode/Railroad.java
@@ -0,0 +1,14 @@
+public class Railroad extends Property {
+  
+  public Railroad (String owner, int cost, int baseRent) {
+    super(owner, cost, baseRent);
+  }
+  
+  public String toString() {
+    return super.toString();
+  }
+  
+  public int calculateRent() {
+    return baseRent * Math.pow(2, owner.getNumRR-1);
+  }
+}
