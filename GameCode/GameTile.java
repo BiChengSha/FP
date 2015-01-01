015a7747a28d257e1a3f565683c958e5612d974c
DavidRadzikovsky <sagadavid@gmail.com>
2015-01-01 13:52:53 -0500

    Create GameTile.java

---
+++ GameCode/GameTile.java
@@ -0,0 +1,13 @@
+// Date: Dec 27, 2014
+
+public class GameTile {
+  protected String name;
+  
+  public GameTile(String name) {
+    this.name = name;
+  }
+  
+  public String toString() {
+    return "Name: " + name;
+  }
+}
