b13cfbe4e1555789dfa7fb174cae6a7a4963009d
Zain <zain.tahlilkar@gmail.com>
2015-01-04 12:30:56 -0500

    Update LuxuryTax.java

--- GameCode/LuxuryTax.java
+++ GameCode/LuxuryTax.java
@@ -3,12 +3,27 @@
 public class LuxuryTax {
   private int amount;
   
+  // accesors
+  public int getAmount() {
+    return amount;
+  }
+  
+  //mutators
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+  
   public LuxuryTax(int amount) {
     super("Luxury Tax");
     this.amount = amount;
   }
   
-  public void deductTax(Player player) {
-    player.cash = player.getCash() - amount;
+  public boolean deductTax(Player player) {
+    if (player.getCash() - amount >= 0) {
+      player.cash = player.getCash() - amount;
+      return true;
+    } else {
+      return false;
+    }
   }
 }
