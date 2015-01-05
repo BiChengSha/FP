bbfa93dac14da3bb349c3af52f88d81f1e41ceed
BiCheng Sha <BiChengSha@users.noreply.github.com>
2015-01-05 10:15:40 -0500

    Update Chance.java

--- GameCode/Chance.java
+++ GameCode/Chance.java
@@ -1,15 +1,20 @@
-import java.util.*;
+/*
+Created by: BiCheng
+Created on: January 5, 2015
+What it does: GameManager will call the ChanceTaken method in this class, and the method will randomly pick a "card" and
+                return the String value (its contents) to the GameManager to handle
+*/
+
+import java.util.*; //For the "Random" class
 
 public class Chance extends GameTile{
   Random rand = new Random();
-  String[] chanceCards = new String[16];
+  String[] chanceCards = new String[16]; //Initialising the array of cards, no values
   int randint;
   
-  
-  
-  public void ChanceTaken(){
-    randint = rand.nextInt(16);
-    return chanceCards[randint];
+  public String ChanceTaken(){
+    randint = rand.nextInt(16); //Calls up a random integer
+    return chanceCards[randint]; //Returns a card value from below
   }
   
   chanceCards[0] = "Advance to Go (Collect $200)";
@@ -30,39 +35,3 @@ public class Chance extends GameTile{
   chanceCards[15] = "You have won a crossword competition - collect $100";
   
 }
-
-/*
-
-Advance to Go (Collect $200)
-
-Advance to Illinois Ave.
-
-Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.
-
-Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank. (There are two of these.)
-
-Advance to St. Charles Place – if you pass Go, collect $200
-
-Bank pays you dividend of $50
-
-Get out of Jail free – this card may be kept until needed, or traded/sold
-
-Go back 3 spaces
-
-Go directly to Jail – do not pass Go, do not collect $200
-
-Make general repairs on all your property – for each house pay $25 – for each hotel $100
-
-Pay poor tax of $15
-
-Take a trip to Reading Railroad – if you pass Go collect $200
-
-Take a walk on the Boardwalk – advance token to Boardwalk
-
-You have been elected chairman of the board – pay each player $50
-
-Your building loan matures – collect $150
-
-You have won a crossword competition - collect $100
-
-*/
