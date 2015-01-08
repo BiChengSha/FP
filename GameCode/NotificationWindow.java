5c2eefb83b1ee5b5a0c62565b04e4544d0660058
IVIacqueen <v_mati2003@hotmail.com>
2015-01-08 11:42:55 -0500

    Update NotificationWindow.java

--- GameCode/NotificationWindow.java
+++ GameCode/NotificationWindow.java
@@ -2,37 +2,52 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-class NotificationWindow extends JFrame
+class NotificationWindow extends JFrame implements ActionListener
 {
-  JPanel panel = new JPanel();
-  JTextArea label;
-  
-  // constructor
-  NotificationWindow( String title, String message )
-  {
-    super( title );                      // invoke the JFrame constructor
-    
-    //setLayout( new FlowLayout() );       // set the layout manager
-    label = new JTextArea(message);  // construct a JLabel
-    label.setEditable(false);
-    label.setBackground(null);
-    label.setLineWrap(true);
-    label.setWrapStyleWord(true);
-    add( label );                        // add the label to the JFrame
-    
-    JButton exit  = new JButton("OK"); // reference to the button object
-    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-    panel.add(label);
-    panel.add(exit);
-    
-    exit.setAlignmentX(Component.CENTER_ALIGNMENT);
-    
-    add( panel );
-    
-    setSize( 400, 175 );
-    setVisible(true);
-    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );   
+   JPanel panel = new JPanel();
+   JTextArea label = new JTextArea(1, 50);
+   JButton exit = new JButton("Okay");
+   
 
-  }
-  
+   NotificationWindow( String title, String message )
+   {
+      super( title );
+      
+      //Sets layout for main panel
+      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); 
+      
+      label.setText(message);       //Adds the desired message to the TextArea
+      label.setEditable(false);             //Sets editable to false so the user cannot change the message whilst in the game
+      label.setBackground(null);            //Sets the background to null so it looks like the message is part of the frame
+      label.setLineWrap(true);              //In case the message goes beyond the size of the frame the rest of the message is pushed below
+      label.setWrapStyleWord(true);
+      
+      //Adds components to the main panel
+      panel.add(label);
+      panel.add(exit);
+      
+      //Aligns components in the center
+      exit.setAlignmentX(Component.CENTER_ALIGNMENT);
+      
+      //Adds panel to the frame
+      add( panel );
+      
+      //Action Listener for the button
+      exit.addActionListener(this);
+      
+      //Set operations
+      setSize( 400, 175 );
+      setVisible(true);
+      setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );   
+   
+   }
+   /**
+    * Action Performed method that disposes of the notification window
+    */
+   public void actionPerformed(ActionEvent evt) {
+     
+      dispose();
+     
+   }
+   
 } 
