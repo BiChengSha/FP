d9e7d2712e2361ce5cd7bcfdd601438f7a538518
Zain <zain.tahlilkar@gmail.com>
2015-01-08 12:13:53 -0500

    Update GameRunner.java

--- GameCode/GameRunner.java
+++ GameCode/GameRunner.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class GameRunner {
   public static void main(String[] args) {
     
+    GameManager manager = new GameManager();
+    
     MainMenu mainWindow = new MainMenu(manager);
     
 
