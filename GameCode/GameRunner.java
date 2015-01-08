c9bf24b84fc5e7773e6de7de5f511c9d2ef3be5f
IVIacqueen <v_mati2003@hotmail.com>
2015-01-08 12:10:58 -0500

    Update GameRunner.java

--- GameCode/GameRunner.java
+++ GameCode/GameRunner.java
@@ -3,25 +3,6 @@ import javax.swing.*;
 public class GameRunner {
   public static void main(String[] args) {
     
-    // System Look and feel
-    try {
-            // Set System L&F
-        UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
-    } 
-    catch (UnsupportedLookAndFeelException e) {
-       // handle exception
-    }
-    catch (ClassNotFoundException e) {
-       // handle exception
-    }
-    catch (InstantiationException e) {
-       // handle exception
-    }
-    catch (IllegalAccessException e) {
-       // handle exception
-    }
-    
     GameManager manager = new GameManager();
     
     MainMenu mainWindow = new MainMenu(manager);
