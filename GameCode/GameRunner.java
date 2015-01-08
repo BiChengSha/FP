332a263578f9b1e2db1c6d48c5ac2cf5ec6c1294
IVIacqueen <v_mati2003@hotmail.com>
2015-01-08 12:11:59 -0500

    Update GameRunner.java

--- GameCode/GameRunner.java
+++ GameCode/GameRunner.java
@@ -3,8 +3,6 @@ import javax.swing.*;
 public class GameRunner {
   public static void main(String[] args) {
     
-    GameManager manager = new GameManager();
-    
     MainMenu mainWindow = new MainMenu(manager);
     
 
