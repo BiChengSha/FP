82f65972e3c6c0e159dcb47656e52097ac76dbce
Zain <zain.tahlilkar@gmail.com>
2015-01-09 18:27:32 -0500

    Update MainMenu.java

--- GameCode/MainMenu.java
+++ GameCode/MainMenu.java
@@ -1,7 +1,5 @@
 /*
  * Name: Matias Gonzalez
- *
- *
  */
 
 
@@ -11,80 +9,72 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MainMenu extends JFrame implements ActionListener {
-   
+
    ImageIcon image = new ImageIcon("title.jpg");
-   
+
    JLabel imageHolder = new JLabel("", image, JLabel.CENTER);
-   
+
    JPanel buttons = new JPanel();
    JPanel position = new JPanel();
-   
+
    JButton newGame = new JButton("New Game");
    JButton loadGame = new JButton("Load Game");
-   
+
    GameManager manager;
-   
+
    public MainMenu(GameManager temp) {
-      
+
       super("Menu");
-      
+
       manager = temp;
-      
+
       newGame.setActionCommand("new");
       loadGame.setActionCommand("load");
-      
+
       position.setLayout(new BoxLayout(position, BoxLayout.Y_AXIS));
-      
+
       //Aligns everything
       newGame.setAlignmentX(Component.CENTER_ALIGNMENT);
       loadGame.setAlignmentX(Component.CENTER_ALIGNMENT);
       imageHolder.setAlignmentX(Component.CENTER_ALIGNMENT);
-      
+
       //Adds all objects to a label
       position.add(imageHolder);
       //Separates image and buttons
       position.add(Box.createRigidArea(new Dimension(1, 50)));
-      
+
       buttons.add(newGame);
       buttons.add(loadGame);
       position.add(buttons);
-      
+
       //Tells these buttons to follow the this actionperformed method
       newGame.addActionListener(this);
       loadGame.addActionListener(this);
-      
+
       //Background color
       buttons.setBackground(Color.WHITE);
       position.setBackground(Color.WHITE);
-      
+
       //Adds the overall layout to the frame
       add(position);
-      
+
       setSize(500, 300);
       setVisible(true);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
+
    }
-   
+
    public void actionPerformed(ActionEvent evt) {
-      
+
       if (evt.getActionCommand().equals("new")) {
+         dispose();
          manager.newGame();
       } else {
-         //manager.loadGame();
+         dispose();
+         manager.loadGame();
       }
-      
+
    }
-   
-   /*
-   public static void main(String[] args) {
-   
-      MainMenu window = new MainMenu("Main Menu");
-      
-      window.setSize(500, 300);
-      window.setVisible(true);
-   
-   }*/
-   
+
 
 }
