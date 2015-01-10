4570517bce5eeda0c65cdf7b1a3e09fd3b8f6c59
Zain <zain.tahlilkar@gmail.com>
2015-01-10 00:18:01 -0500

    Update GameWindow.java

--- GameCode/GameWindow.java
+++ GameCode/GameWindow.java
@@ -8,37 +8,37 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GameWindow extends JFrame implements ActionListener{
+public class GameWindow extends JFrame implements ActionListener {
   /**
    * The width of the main window
    */
   private int sizex = 700;
-  
+
   /**
    * Height of the main window
    */
   private int sizey = 900;
-  
+
   /**
-   * Game Manager instance 
+   * Game Manager instance
    */
   GameManager manager;
-  
+
   /**
    * Variables for drawing board
    */
-  
+
   private JPanel board;
   private JLabel imageHolder;
   private ImageIcon image;
-  
+
   /**
    * Variables for Drawing options
    */
   private JPanel holder;
   private JPanel infoPanel;
   private JButton purchase;
-  
+
   private JPanel optionButtons;
   private JButton roll;
   private JButton endTurn;
@@ -47,13 +47,13 @@ public class GameWindow extends JFrame implements ActionListener{
   private JButton playerStats;
   private JButton saveGame;
   private JButton endGame;
-  
+
   /**
    * Text Area to put property info into
    * Can be edited by external classes to update the text it shows.
    */
   public JTextArea propertyInfo;
-  
+
   /**
    * Class Constructor
    * Creates the window, draws the board and buttons
@@ -69,16 +69,16 @@ public class GameWindow extends JFrame implements ActionListener{
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setResizable(true);
     setLayout(new FlowLayout());
-    
+
     // drawing the game board
     drawBoard();
-    
+
     // drawing game options section (under board)
     drawOptions();
-    
+
     setVisible(true);
   }
-  
+
   /**
    * Draws Game Board Panel
    * @author Zain
@@ -88,21 +88,21 @@ public class GameWindow extends JFrame implements ActionListener{
     board = new JPanel(new BorderLayout());
     image = new ImageIcon("MonopolyBoard500.jpg");
     imageHolder = new JLabel("", image, JLabel.CENTER);
-    
+
     board.setMinimumSize(new Dimension(sizex, (int)(2*sizey/3)));
     // add image to jpanel
     board.add(imageHolder/*, BorderLayout.CENTER*/);
     // add Jpanel to JFrame
     add(board);
   }
-  
+
   /**
    * Draws the game options panel
    * @author Zain
    */
   public void drawOptions() {
     holder = new JPanel(new BorderLayout());
-    
+
     infoPanel = new JPanel();
     infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.PAGE_AXIS));
     // 3/4 the width and half the height
@@ -118,12 +118,13 @@ public class GameWindow extends JFrame implements ActionListener{
     infoPanel.add(purchase);
 
     holder.add(infoPanel, BorderLayout.LINE_START);
-    
+
     // Now for the buttons
     // components
     optionButtons = new JPanel();
     optionButtons.setLayout(new BoxLayout(optionButtons, BoxLayout.PAGE_AXIS));
     roll = new JButton("Roll");
+    roll.setAlignmentX(Component.CENTER_ALIGNMENT);
     viewAllProperties = new JButton("View All Properties");
     viewAllProperties.setAlignmentX(Component.CENTER_ALIGNMENT);
     viewOwnedProperties = new JButton("View Owned Properties");
@@ -136,9 +137,9 @@ public class GameWindow extends JFrame implements ActionListener{
     saveGame.setAlignmentX(Component.CENTER_ALIGNMENT);
     endGame = new JButton("End Game");
     endGame.setAlignmentX(Component.CENTER_ALIGNMENT);
-    
+
     optionButtons.setSize(sizex / 4, sizey / 2);
-    
+
     // Action commands
     roll.setActionCommand("roll");
     purchase.setActionCommand("purchase");
@@ -148,7 +149,7 @@ public class GameWindow extends JFrame implements ActionListener{
     playerStats.setActionCommand("playerStats");
     saveGame.setActionCommand("saveGame");
     endGame.setActionCommand("endGame");
-    
+
     // Adding action listeners to buttons
     roll.addActionListener(this);
     purchase.addActionListener(this);
@@ -158,7 +159,7 @@ public class GameWindow extends JFrame implements ActionListener{
     playerStats.addActionListener(this);
     saveGame.addActionListener(this);
     endGame.addActionListener(this);
-    
+
     // adding everything
     optionButtons.add(roll);
     optionButtons.add(endTurn);
@@ -167,52 +168,58 @@ public class GameWindow extends JFrame implements ActionListener{
     optionButtons.add(playerStats);
     optionButtons.add(saveGame);
     optionButtons.add(endGame);
-    
+
     holder.add(optionButtons, BorderLayout.LINE_END);
-    
+
     add(holder);
   }
-  
-  
+
+
   /**
    * Handles all the events (button clicks, etc)
    * @author Zain
    */
   public void actionPerformed(ActionEvent e) {
-    String command = e.getActionCommand();
-    
-    if (command.equals("roll")) {
-      //Roll dice
-      
-      
-      manager.rollDice();  
-      
-      if (!manager.rollCheck()) {
-        roll.setEnabled(false);
-      }
-      
-      new NotificationWindow("Rock n roll bitch", "You rolled a " + manager.getDie1().getLastRoll() + " and " + manager.getDie2().getLastRoll());
-      
+    	String command = e.getActionCommand();
+
+    	if (command.equals("roll")) {
+      	//Roll dice
+
+			manager.rollDice();
+
+			if (!manager.rollCheck()) {
+	   		roll.setEnabled(false);
+	   		endTurn.setEnabled(true);
+    		}
+
+// 		if (!manager.doubles()) {
+// 			roll.setEnabled(false);
+// 			endTurn.setEnabled(true);
+// 		}
+
+      	new NotificationWindow("Rock n roll bitch", "You rolled a " + manager.getDie1().getLastRoll() + " and " + manager.getDie2().getLastRoll());
+
     } else if (command.equals("purchase")) {
       // purchase button
-      
+
+
     } else if (command.equals("viewAllProp")) {
       // opening the property window
       new PropertyWindow(manager.getPropertyManager());
-      
-    } else if (command.equals("viewOwnedProp")) {      
+
+    } else if (command.equals("viewOwnedProp")) {
       // getting the current player's property manager
       PropertyManager propManager = manager.getPlayers()[manager.getCurrentPlayerIndex()].getPropertiesOwned();
-      
+
       // opening the property window
       new PropertyWindow(propManager);
-      
+
     } else if (command.equals("playerStats")) {
       // Player info
       PlayerInfo playerInfo = new PlayerInfo(manager);
-      
+
     } else if (command.equals("endTurn")) {
-      
+
       /*if (rollCheck && payCheck && manager.players[manager.getCurrentPlayerIndex()].getCash() >= 0) {
          manager.endTurn();
       } else if (rollCheck && payCheck) {
@@ -222,19 +229,19 @@ public class GameWindow extends JFrame implements ActionListener{
       } else {
          new NotificationWindow("Warning", "You haven't rolled yet, you dingus");
       }*/
-      
+
       roll.setEnabled(true);
+      endTurn.setEnabled(false);
       manager.endTurn();
-      
+
     } else if (command.equals("saveGame")) {
       // save the game
       manager.save();
-      
+
     } else if (command.equals("endGame")) {
       // end the game
       System.exit(0);
     }
-               
+
   }
 }
-
