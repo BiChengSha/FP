73ebe415939e54c07d21815a89b44e1d1a8634bc
Zain <zain.tahlilkar@gmail.com>
2015-01-10 00:16:48 -0500

    Update GameRunner.java

--- GameCode/GameRunner.java
+++ GameCode/GameRunner.java
@@ -1,12 +1,32 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class GameRunner {
   public static void main(String[] args) {
-    
-    GameManager manager = new GameManager();
-    
-    MainMenu mainWindow = new MainMenu(manager);
-    
+  // code copied from oracle website
+  	 try {
+            // Set cross-platform Java L&F (also called "Metal")
+        UIManager.setLookAndFeel(
+            UIManager.getCrossPlatformLookAndFeelClassName());
+    }
+    catch (UnsupportedLookAndFeelException e) {
+       // handle exception
+    }
+    catch (ClassNotFoundException e) {
+       // handle exception
+    }
+    catch (InstantiationException e) {
+       // handle exception
+    }
+    catch (IllegalAccessException e) {
+       // handle exception
+    }
+
+	 GameManager manager = new GameManager();
+
+	 MainMenu mainWindow = new MainMenu(manager);
+
+	 mainWindow.pack();
 
   }
 }
